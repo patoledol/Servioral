69b7f2307b2f2cbd9440901f88c8cf820db41ce7
Juan Pablo Moreno <63758089+JuanPabloMoreno971229@users.noreply.github.com>
2022-05-10 21:50:52 -0500

    Update models.py

--- servioral/Cita/models.py
+++ servioral/Cita/models.py
@@ -1,12 +1,18 @@
 import imp, datetime
 from django.db import models
 from Odontologo.models import odontologo
+from Especialidad.models import especialidad
+from Paciente.models import paciente
+
 # Create your models here.
 
 class cita(models.Model):
 
+    status=models.BooleanField(default=False, verbose_name="Aceptada")
     id_cita = models.AutoField(primary_key=True, verbose_name="Id acudiente")
-    id_doc = models.ForeignKey(odontologo, null= True, blank= True, on_delete=models.CASCADE )
+    paciente = models.ForeignKey(paciente, null= True, blank= True, on_delete=models.CASCADE, verbose_name="Paciente" )
+    id_doc = models.ForeignKey(odontologo, null= True, blank= True, on_delete=models.CASCADE, verbose_name="Doctor" )
+    espe = models.ForeignKey(especialidad,null= True, blank= True, on_delete=models.CASCADE, verbose_name="Especialidad" )
     day = datetime.datetime.now()
     formatedDay  = day.strftime("%Y/%m/%d")
     date = models.DateTimeField()
@@ -18,5 +24,5 @@ class cita(models.Model):
         verbose_name = "Cita"
         verbose_name_plural = "Citas"
                 
-    def __str__(self):
-        return self.name
+    # def __str__(self):
+    #     return self.name
